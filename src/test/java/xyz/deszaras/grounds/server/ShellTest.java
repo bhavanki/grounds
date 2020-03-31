ecc0db6ccaf7e38a2fcc66eb54589df359661b35
Bill Havanki <kbzodnj@gmail.com>
2020-03-30 20:26:25 -0400

    Fix ShellTest

--- src/test/java/xyz/deszaras/grounds/server/ShellTest.java
+++ src/test/java/xyz/deszaras/grounds/server/ShellTest.java
@@ -2,21 +2,38 @@ package xyz.deszaras.grounds.server;
 
 import static org.junit.Assert.assertEquals;
 
-import com.google.common.collect.ImmutableList;
-import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
+import xyz.deszaras.grounds.model.Player;
+import xyz.deszaras.grounds.model.Universe;
 
 public class ShellTest {
 
-  @Test
-  public void testTokenizeBasic() {
-    List<String> tokens = Shell.tokenize("a b c");
-    assertEquals(ImmutableList.of("a", "b", "c"), tokens);
+  private Player player;
+
+  @Before
+  public void setUp() {
+    Universe universe = new Universe("test");
+    player = new Player("testPlayer", universe);
   }
 
   @Test
-  public void testTokenizeWithDoubleQuotes() {
-    List<String> tokens = Shell.tokenize("a \"b c\" d");
-    assertEquals(ImmutableList.of("a", "b c", "d"), tokens);
+  public void testPreprocessNothing() {
+    String line = "Hi there.";
+    assertEquals(line, Shell.preprocess(line, player));
   }
+
+  @Test
+  public void testPreprocessPose() {
+    String line = ":dances.";
+    assertEquals("POSE " + player.getName() + " dances.",
+                 Shell.preprocess(line, player));
+  }
+
+  @Test
+  public void testPreprocessEmpty() {
+    String line = "";
+    assertEquals(line, Shell.preprocess(line, player));
+  }
+
 }
