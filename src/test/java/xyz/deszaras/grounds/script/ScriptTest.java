a9ad4a535ff6d79697031c3ab2b09d841585e568
Bill Havanki <kbzodnj@gmail.com>
2020-05-24 19:27:08 -0400

    Add ScriptTest

---
+++ src/test/java/xyz/deszaras/grounds/script/ScriptTest.java
@@ -0,0 +1,27 @@
+package xyz.deszaras.grounds.script;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.junit.Test;
+import xyz.deszaras.grounds.model.Extension;
+import xyz.deszaras.grounds.model.Player;
+import xyz.deszaras.grounds.model.Universe;
+
+public class ScriptTest {
+
+  @Test
+  public void testGetters() {
+    String content = "println('Hello world')";
+    Player owner = new Player("owner", Universe.VOID);
+    Extension extension = new Extension("extension", Universe.VOID);
+
+    Script s = new Script(content, owner, extension);
+
+    assertEquals(content, s.getContent());
+    assertEquals(owner, s.getOwner());
+    assertEquals(extension, s.getExtension());
+  }
+
+}
