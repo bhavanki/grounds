f4e3abaa40890216ec9c97b0ffa93c093f87440e
Bill Havanki <kbzodnj@gmail.com>
2020-05-26 13:20:43 -0400

    Starting attempt to sandbox scripts
    Scripts should not be trusted code, and the groovy-sandbox project aims
    to have scripts run in a sandbox so that they can't, say, call
    System.exit or use reflection or do other things. I tried to integrate
    it, but it's not working out.
    
    - The sandbox library requires Groovy 2.
    - For some reason, in my tests, my (chat) script needs java.lang.Class,
    which is a bad idea to allow.
    - The script also needs not just String, but arrays of String, which is
    a separate class.
    
    At the moment, sandboxing seems intractable.

--- src/main/java/xyz/deszaras/grounds/script/ScriptCallable.java
+++ src/main/java/xyz/deszaras/grounds/script/ScriptCallable.java
@@ -1,12 +1,16 @@
 package xyz.deszaras.grounds.script;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import org.codehaus.groovy.control.CompilerConfiguration;
+import org.kohsuke.groovy.sandbox.GroovyValueFilter;
+import org.kohsuke.groovy.sandbox.SandboxTransformer;
 import xyz.deszaras.grounds.command.Actor;
 import xyz.deszaras.grounds.model.Player;
 
@@ -59,20 +63,70 @@ public class ScriptCallable implements Callable<String> {
     // Compile the script in a Groovy shell.
     CompilerConfiguration compilerConfig = new CompilerConfiguration();
     compilerConfig.setScriptBaseClass(GroundsScript.class.getName());
+    compilerConfig.addCompilationCustomizers(new SandboxTransformer());
 
     GroovyShell commandShell = new GroovyShell(binding, compilerConfig);
+
+    GroovyScriptSandbox sandbox = new GroovyScriptSandbox();
+    sandbox.register();
+
     GroundsScript gscript = (GroundsScript) commandShell.parse(script.getContent());
     gscript.setActor(actor);
     gscript.setPlayer(player);
     gscript.setOwner(script.getOwner());
 
     // Run it!
-    Object result = gscript.run();
+    Object result;
+    try {
+      result = gscript.run();
+    } finally {
+      sandbox.unregister();
+    }
 
     if (result == null) {
       return null;
     } else {
       return result.toString();
     }
+  }
+
+  private static class GroovyScriptSandbox extends GroovyValueFilter {
+    @Override
+    public Object filter(Object o) {
+      if (o == null) {
+        return o;
+      }
+      if (o instanceof groovy.lang.Script ||
+          o instanceof groovy.lang.Closure) {
+        return o;
+      }
+      if (ALLOWED_TYPES.contains(o.getClass())) {
+        return o;
+      }
+
+      // System.err.println("Need to access " + o.getClass());
+      // return o;
+
+      throw new SecurityException("Scripts may not work with class " +
+                                  o.getClass().getName());
+    }
+
+    private static final Set<Class<?>> ALLOWED_TYPES =
+        ImmutableSet.<Class<?>>builder()
+          .add(String.class) // need arrays too
+          .add(Boolean.class)
+          .add(Integer.class)
+          .add(Long.class)
+
+          .add(java.util.Optional.class)
+          .add(java.util.HashMap.class)
+
+          .add(org.codehaus.groovy.runtime.GStringImpl.class)
+
+          .add(xyz.deszaras.grounds.model.Attr.class)
+          .add(GroundsScript.class)
+
+          .add(Class.class) // NO
+          .build();
   }
 }
