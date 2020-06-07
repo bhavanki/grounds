d81cda71aa88b6837c9be2e2ac2a46eaac3487aa
Bill Havanki <kbzodnj@gmail.com>
2020-06-07 15:54:44 -0400

    Add help for IndexCommand

--- src/main/java/xyz/deszaras/grounds/command/IndexCommand.java
+++ src/main/java/xyz/deszaras/grounds/command/IndexCommand.java
@@ -64,4 +64,9 @@ public class IndexCommand extends Command<String> {
     }
     return new IndexCommand(actor, player, Multiverse.MULTIVERSE.getUniverse(name));
   }
+
+  public static String help() {
+    return "INDEX <universe>\n\n" +
+        "Lists all things contained in a universe";
+  }
 }
