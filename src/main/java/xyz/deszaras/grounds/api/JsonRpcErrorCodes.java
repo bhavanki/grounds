db465335443739e744ed31c078da05bb3ce62791
Bill Havanki <kbzodnj@gmail.com>
2022-03-20 20:56:44 -0400

    Define NOT_FOUND JSON RPC error code
    Like a 404 but for API calls.

--- src/main/java/xyz/deszaras/grounds/api/JsonRpcErrorCodes.java
+++ src/main/java/xyz/deszaras/grounds/api/JsonRpcErrorCodes.java
@@ -1,7 +1,7 @@
 package xyz.deszaras.grounds.api;
 
 /**
- * Standard JSON RPC error codes.
+ * Standard and Grounds-specific JSON RPC error codes.
  */
 public final class JsonRpcErrorCodes {
 
@@ -10,6 +10,8 @@ public final class JsonRpcErrorCodes {
   public static final int METHOD_NOT_FOUND = -32601;
   public static final int INVALID_PARAMETERS = -32602;
   public static final int INTERNAL_ERROR = -32603;
+
+  public static final int NOT_FOUND = -32004;
 
   private JsonRpcErrorCodes() {
   }
