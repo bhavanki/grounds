9e551b4b04a70b1f035ba116a394e7a205476c1e
Bill Havanki <kbzodnj@gmail.com>
2022-04-03 21:20:43 -0400

    Remove temporary pg_ prefixes from magicfiddle plugin

--- plugins/magicfiddle/plugin_magicfiddle.cmd
+++ plugins/magicfiddle/plugin_magicfiddle.cmd
@@ -7,14 +7,14 @@
 # Run as GOD where you want the magic fiddle to live. Adjust the paths to YAML
 # files as necessary for them to load.
 
-build extension pg_magic_fiddle_ext
-yoink pg_magic_fiddle_ext here
-set_attr pg_magic_fiddle_ext ^welcome[ATTRLIST]=@plugins/magicfiddle/magicfiddle.yaml
-role add bard pg_magic_fiddle_ext
+build extension magic_fiddle_ext
+yoink magic_fiddle_ext here
+set_attr magic_fiddle_ext ^welcome[ATTRLIST]=@plugins/magicfiddle/magicfiddle.yaml
+role add bard magic_fiddle_ext
 
-build thing "pg magic fiddle"
+build thing "magic fiddle"
 # stop anyone from picking it up
-change_policy "pg magic fiddle" g-d
-describe "pg magic fiddle" "This faintly sparkling, faintly golden musical instrument hovers steadily in the corner, its bow trained across its strings."
+change_policy "magic fiddle" g-d
+describe "magic fiddle" "This faintly sparkling, faintly golden musical instrument hovers steadily in the corner, its bow trained across its strings."
 
 say Magic fiddle installed.
